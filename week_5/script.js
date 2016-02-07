99bb23cc03fc372959a17a1b546a60de0aa30467
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-02-06 20:06:41 -0800

    Refactor

--- week_5/script.js
+++ week_5/script.js
@@ -1,44 +1,18 @@
-var selected = null;
-
-// build table
-
-var table = document.createElement("table");
-document.body.appendChild(table);
-
-var thead = document.createElement("thead");
-table.appendChild(thead);
-
-var headTr = document.createElement("tr");
-thead.appendChild(headTr);
-
-for (var i = 1; i < 5; i++) {
-	var td = document.createElement("td");
-	td.textContent = "Header " + i;
-	headTr.appendChild(td);
+// mark currently selected node
+function markSelectedNode() {
+	selectedNode.style.backgroundColor = "yellow";
 }
 
-var tbody = document.createElement("tbody");
-table.appendChild(tbody);
-
-for (var i = 1; i < 5; i++) {
-	var bodyTr = document.createElement("tr");
-	tbody.appendChild(bodyTr);
-	for (var n = 1; n < 5; n++) {
-		var td = document.createElement("td");
-		td.textContent = n + ', ' + i;
-		bodyTr.appendChild(td);
-	}
-}
-
-// select/mark functionality
+// select a node
 function select(el) {
-	if (selected) {
-		selected.style.outline = "none";
+	if (selectedNode) {
+		selectedNode.style.outline = "none";
 	}
-	selected = el;
+	selectedNode = el;
 	el.style.outline = "1px solid black";
 }
 
+// get node's index relative to siblings
 function getIndex(e) {
 	var parent = e.parentNode;
 	for (var i = 0; i < parent.children.length; i++) {
@@ -48,45 +22,105 @@ function getIndex(e) {
 	}
 }
 
+// move selection outline in one of four directions
 function moveSelection(event) {
 	var direction = event.target.id;
 	switch(direction) {
 		case "up":
-			var aboveTr = selected.parentNode.previousElementSibling;
-		    if (aboveTr) {
-		    	prevSelectedIdx = getIndex(selected);
-		    	select(aboveTr.children[prevSelectedIdx]);
-			}
-			break;
+		var aboveTr = selectedNode.parentNode.previousElementSibling;
+		if (aboveTr) {
+			prevSelectedIdx = getIndex(selectedNode);
+			select(aboveTr.children[prevSelectedIdx]);
+		}
+		break;
 		case "down":
-			var belowTr = selected.parentNode.nextElementSibling;
-			if (belowTr) {
-				prevSelectedIdx = getIndex(selected);
-		    	select(belowTr.children[prevSelectedIdx]);
-			}
-			break;
-		case "left":
-			if (selected.nextElementSibling) {
-				select(selected.nextElementSibling);
-			}
-			break;
+		var belowTr = selectedNode.parentNode.nextElementSibling;
+		if (belowTr) {
+			prevSelectedIdx = getIndex(selectedNode);
+			select(belowTr.children[prevSelectedIdx]);
+		}
+		break;
 		case "right":
-			if (selected.previousElementSibling) {
-				select(selected.previousElementSibling);
-			}
-			break;
+		var rightSibling = selectedNode.nextElementSibling
+		if (rightSibling) {
+			select(rightSibling);
+		}
+		break;
+		case "left":
+		var leftSibling = selectedNode.previousElementSibling;
+		if (leftSibling) {
+			select(leftSibling);
+		}
+		break;
 	}
 }
 
-// build directional buttons
+// build table - function is probably unnecessary,
+// but why clutter up a namespace
+function buildTable() {
+	// create and add table
+	var table = document.createElement("table");
+	document.body.appendChild(table);
 
-labels = ["up", "down", "left", "right"];
-for (var i = 0; i < labels.length; i++) {
-	var button = document.createElement("button");
-	button.setAttribute('id', labels[i]);
-	button.textContent = labels[i];
-	button.addEventListener("click", moveSelection)
-	document.body.appendChild(button);
+	// create and add header cells
+	var thead = document.createElement("thead");
+	table.appendChild(thead);
+
+	var headTr = document.createElement("tr");
+	thead.appendChild(headTr);
+
+	for (var i = 1; i < 5; i++) {
+		var td = document.createElement("td");
+		td.textContent = "Header " + i;
+		headTr.appendChild(td);
+	}
+
+	// create and add remaining cells
+	var tbody = document.createElement("tbody");
+	table.appendChild(tbody);
+
+	for (var i = 1; i < 5; i++) {
+		var bodyTr = document.createElement("tr");
+		tbody.appendChild(bodyTr);
+		for (var n = 1; n < 5; n++) {
+			var td = document.createElement("td");
+			td.textContent = n + ', ' + i;
+			bodyTr.appendChild(td);
+		}
+	}
+
+	// select the cell at 1,1
+	select(tbody.firstElementChild.firstElementChild);
 }
 
-select(tbody.firstElementChild.firstElementChild);
+// build directional buttons
+function buildDirectionButtons() {
+	var labels = ["up", "down", "left", "right"];
+	for (var i = 0; i < labels.length; i++) {
+		var button = document.createElement("button");
+		button.setAttribute('id', labels[i]);
+		button.textContent = labels[i];
+		button.addEventListener("click", moveSelection)
+		document.body.appendChild(button);
+	}
+}
+
+// build button that marks cells
+function buildMarkButton() {
+	var markButton = document.createElement('button');
+	markButton.textContent = "Mark Cell";
+	markButton.setAttribute('id', 'cell-marker');
+	document.body.appendChild(markButton);
+	markButton.addEventListener('click', markSelectedNode);
+}
+
+var selectedNode = null;
+buildTable();
+buildDirectionButtons();
+buildMarkButton();
+
+
+
+
+
+
