b29583c3a709d0b49e884f32f87f9a12904c174f
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-02-06 19:53:15 -0800

    Enable button navigation

--- week_5/script.js
+++ week_5/script.js
@@ -1,4 +1,6 @@
-console.log("hi");
+var selected = null;
+
+// build table
 
 var table = document.createElement("table");
 document.body.appendChild(table);
@@ -28,17 +30,63 @@ for (var i = 1; i < 5; i++) {
 	}
 }
 
-function selectCell(x, y) {
-	// deselect old cell
-    var previouslySelectedCell = document.getElementsByClassName("selected")[0];
-    if (previouslySelectedCell) {
-    	previouslySelectedCell.classList.toggle("selected");
-    }
-    
-    // select new cell
-	var row = tbody.children[y - 1];
-	var cell = row.children[x - 1];
-	cell.classList.toggle("selected");
+// select/mark functionality
+function select(el) {
+	if (selected) {
+		selected.style.outline = "none";
+	}
+	selected = el;
+	el.style.outline = "1px solid black";
 }
 
-selectCell(1,1);
+function getIndex(e) {
+	var parent = e.parentNode;
+	for (var i = 0; i < parent.children.length; i++) {
+		if (parent.children[i] === e) {
+			return i;
+		}
+	}
+}
+
+function moveSelection(event) {
+	var direction = event.target.id;
+	switch(direction) {
+		case "up":
+			var aboveTr = selected.parentNode.previousElementSibling;
+		    if (aboveTr) {
+		    	prevSelectedIdx = getIndex(selected);
+		    	select(aboveTr.children[prevSelectedIdx]);
+			}
+			break;
+		case "down":
+			var belowTr = selected.parentNode.nextElementSibling;
+			if (belowTr) {
+				prevSelectedIdx = getIndex(selected);
+		    	select(belowTr.children[prevSelectedIdx]);
+			}
+			break;
+		case "left":
+			if (selected.nextElementSibling) {
+				select(selected.nextElementSibling);
+			}
+			break;
+		case "right":
+			if (selected.previousElementSibling) {
+				select(selected.previousElementSibling);
+			}
+			break;
+	}
+}
+
+// build directional buttons
+
+labels = ["up", "down", "left", "right"];
+for (var i = 0; i < labels.length; i++) {
+	var button = document.createElement("button");
+	button.setAttribute('id', labels[i]);
+	button.textContent = labels[i];
+	button.addEventListener("click", moveSelection)
+	document.body.appendChild(button);
+}
+
+select(tbody.firstElementChild.firstElementChild);
