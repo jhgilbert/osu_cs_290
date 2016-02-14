3738bb077f00bbaa9b7c39489040767fef76ff48
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-02-14 00:10:31 -0800

    Finish basic Open Weather Map AJAX functionality

--- week_6/script.js
+++ week_6/script.js
@@ -1,12 +1,37 @@
 var OPEN_WEATHER_MAP_KEY = '883dd2f4f91dc1a2b2580c100ea0389a';
+var weatherApiUrl = null;
 
 document.addEventListener('DOMContentLoaded', initPostFormListener);
 document.addEventListener('DOMContentLoaded', initWeatherFormListener);
+document.addEventListener('DOMContentLoaded', forceSingleWeatherFormInput);
+
+function forceSingleWeatherFormInput () {
+	document.getElementById('weather-city').oninput = function() {
+		document.getElementById('weather-zip').value = "";
+		weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + this.value + ",us&appid=" + OPEN_WEATHER_MAP_KEY;
+	}
+	document.getElementById('weather-zip').oninput = function() {
+		document.getElementById('weather-city').value = "";
+		weatherApiUrl = "http://api.openweathermap.org/data/2.5/weather?zip=" + this.value + ",us&appid=" + OPEN_WEATHER_MAP_KEY;
+	}
+}
 
 function initWeatherFormListener() {
 	document.getElementById('weather-request').addEventListener('submit', function() {
 		event.preventDefault();
-		// do your thing
+		var req = new XMLHttpRequest();
+		req.open("GET", weatherApiUrl, true);
+		req.addEventListener('load', function() {
+			var response = JSON.parse(req.responseText);
+			if (response.cod == "200") {
+				var response = JSON.parse(req.responseText);
+				document.getElementById('weather-response-content').textContent = JSON.stringify(response);
+			} else {
+				document.getElementById('weather-response-content').textContent = '';
+				alert(response.message);
+			}
+		});
+		req.send(null);
 	})
 }
 
