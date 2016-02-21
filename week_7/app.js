4eb670e1c94f2a3c8dbb1aa8741a844424dc404a
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-02-20 22:14:46 -0800

    Add basic checker functionality

--- week_7/app.js
+++ week_7/app.js
@@ -1,17 +1,38 @@
 var express = require('express');
 
 var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', 3000);
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', function(req,res) {
-  res.type('text/plain');
-  res.send('This was a GET request.');
+  context = {requestType: 'GET'};
+  if (Object.keys(req.query).length) {
+    context.queryParams = req.query;
+  }
+  if (Object.keys(req.body).length) {
+    context.bodyParams = req.body;
+  }
+  res.render('request-report', context);
 });
 
 app.post('/', function(req,res) {
-  res.type('text/plain');
-  res.send('This was a POST request.');
+  context = {requestType: 'POST'};
+  if (Object.keys(req.query).length) {
+    context.queryParams = req.query;
+  }
+  if (Object.keys(req.body).length) {
+    context.bodyParams = req.body;
+  }
+  res.render('request-report', context);
 });
 
 app.use(function(req,res) {
