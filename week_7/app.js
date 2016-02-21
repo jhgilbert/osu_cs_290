7e3abc0ac831b7ebc523d8329d07017233a83d99
Jennifer Gilbert <jennifer@devbootcamp.com>
2016-02-20 21:28:44 -0800

    Get basic POST and GET routes working

---
+++ week_7/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', 3000);
+
+app.get('/', function(req,res) {
+  res.type('text/plain');
+  res.send('This was a GET request.');
+});
+
+app.post('/', function(req,res) {
+  res.type('text/plain');
+  res.send('This was a POST request.');
+});
+
+app.use(function(req,res) {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl+C to terminate.');
+});
